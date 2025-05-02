36c48bda7c9250f759adf34cce341e59708b9e92
Bustah <psnatb2lme@gmail.com>
2025-05-02 12:11:30 +0800

    added frames relative to each page

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -2,8 +2,10 @@ import React, { useState } from 'react'
 
 function App() {
 
-  const [pages, setPages] = useState([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0])
-  const [frames, setFrames] = useState([3,4,5])
+  const [pages, setPages] = useState([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]);
+  const pageNumber = 0;
+  const [frames, setFrames] = useState([0,0,0]);
+  const [framesNum, setFramesNum] = useState([3,4,5]);
   const [faults, setFaults] = useState(0);
 
   function handleChangeValue(index){
@@ -32,8 +34,9 @@ function App() {
 
   return (
     <section className='bg-gray-300 w-full h-full '>
-    <div className='w-full h-1/12 bg-gray-600 border-4 justify-center items-center flex'>
+    <div className='w-full h-1/12 bg-gray-600 border-4 justify-center items-center flex relative'>
         <h1 className='text-white titleText'>FIFO ALGORITHM</h1>
+        <p className='absolute bottom-0 right-2 text-white'>white = page, dark gray = frame</p>
     </div>
 
     <div className='h-11/12 w-full bg-white relative'>
@@ -44,19 +47,25 @@ function App() {
 
         <h1 className='buttonText text-center mt-10'>No. of Frames</h1>
 
-        {frames.map((frame, _) =>
-        <button className='w-12/12 buttonText h-1/12 mb-4 rounded-2xl bg-gray-300 buttonText border-3'>{frame}</button>)}
+        {framesNum.map((frameNum, _) =>
+        <button className='w-12/12 buttonText h-1/12 mb-4 rounded-2xl bg-gray-300 buttonText border-3'>{frameNum}</button>)}
 
         <h1 className='buttonText text-black text-center mt-1'>No. of Faults:</h1>
-        <h1 className='buttonText text-black text-center mt-1 text-red-700 font-bold'>{faults}</h1>
+        <h1 className='buttonText text-center mt-1 text-red-700 font-bold'>{faults}</h1>
       </div>
 
-      <div className='h-full md:w-10/12 bg-gray-400 border-4 border-t-0 border-l-0 absolute right-0 w-7/12 justify-center grid grid-cols-5 lg:grid-cols-20 md:grid-cols-10 '>
+      <div className='h-full md:w-10/12 bg-gray-400 border-4 border-t-0 border-l-0 absolute right-0 w-7/12 justify-center grid grid-cols-5 md:grid-cols-10 '>
 
         {pages.map((page, index)=>
-        <div className='bg-gray-300 ml-2 sm:ml-1 md:ml-3 w-fit h-fit p-2 rounded-2xl mt-2 text-7xl pageReference' key={index} onClick={() => handleChangeValue(index)}>
-          {page}
-        </div>)}
+            <div className='flex-col border justify-center items-center'>
+                <div className='bg-gray-300 ml-auto mr-auto  w-fit h-fit p-2 rounded-3xl mt-2 text-7xl pageReference' key={index} onClick={() => handleChangeValue(index)}>
+                  {page}
+                </div>
+              {frames.map((frame,i)=>
+                <div className='bg-gray-500 ml-auto mr-auto  w-fit h-fit p-2 rounded mt-2 text-7xl frameText'>
+                  {frame}
+                </div>)}
+            </div>)}
 
       </div>
 
